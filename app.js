be0edcd3a331ed925ab441f4f33be3ef9e2c6748
greatapps <salokya01@gmail.com>
2020-11-06 01:30:43 +0530

    Reverted to intial changes

--- app.js
+++ app.js
@@ -78,8 +78,6 @@ app.use(jwt());
 require("./router")(app);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next(createError(404));
 });
 // error handler
