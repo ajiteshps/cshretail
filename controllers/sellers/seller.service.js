95364a5f6c3f9166c0a7cb452d29a47638d29818
Prince Singh <prince.singh@telaminfotech.com>
2020-11-18 19:00:56 +0530

    checking

--- controllers/sellers/seller.service.js
+++ controllers/sellers/seller.service.js
@@ -353,11 +353,10 @@ async function insertRecord(fieldsToBeInsert, fieldValues, tableName){
 
 
 async function add(req) {
-    console.log(req.body,'DATA FROM Frontend');
     try {
         if (!_.isEmpty(req.body)) {
             // Insert new Delear
-            
+           
             is_Validate = true;
             is_Validate = is_Validate ? validation.issetNotEmpty(req.body.name) : false;
             is_Validate = is_Validate ? validation.issetNotEmpty(req.body.mobile_contact__c) : false;
@@ -436,9 +435,9 @@ async function add(req) {
                         
             
 
-                fieldsToBeInsert = ' asm__c, name, type1__c, email__c, mobile_contact__c, potential_value__c, potential_retailer__c,category__c,retailer_category__c,createddate,competitor__c,owner_name__c,owner_phone__c,gstin__c,billingcity,billingstreet,billingpostalcode,billingcountry,billingstate,area__c,psm__c';
+                fieldsToBeInsert = ' asm__c, name,   type1__c, email__c, mobile_contact__c, potential_value__c, potential_retailer__c,category__c,retailer_category__c,createddate,competitor__c,owner_name__c,owner_phone__c,gstin__c,billingcity,billingstreet,billingpostalcode,billingcountry,billingstate,area__c,psm__c';
                 pg_id__c = uuidv4();
-                fieldValues = [ asm_id, req.body.name, '0122w000000NfMlAAK', email__c, mobile_contact__c, potential_value__c, potential_retailer__c, category__c, retailer_category__c, current_date_time, competitor__c, owner_name__c, owner_phone__c, gstin__c, billingcity, billingstreet, billingpostalcode, billingcountry, billingstate,area__c,psm_id];
+                fieldValues = [ asm_id, req.body.name, 'Retailer', email__c, mobile_contact__c, potential_value__c, potential_retailer__c, category__c, retailer_category__c, current_date_time, competitor__c, owner_name__c, owner_phone__c, gstin__c, billingcity, billingstreet, billingpostalcode, billingcountry, billingstate,area__c,psm_id];
                 
                 tableName = 'Account';
 
@@ -453,12 +452,12 @@ async function add(req) {
 
                 }else{
                     response.status = 400;
-                    response.response = { "success": false, "message": "Error while create record. Please try again."};
+                    response.response = { "success": false, "message": "Error while create record. Please try again.", "data": {} };
                 }
                 return response;
             }else{
                 response.status = 400;
-                response.response = { "success": false, "message": "Mandatory parameter(s) are missing." };
+                response.response = { "success": false, "message": "Mandatory parameter(s) are missing.", "data": {} };
                 return response;     
             }
         }
