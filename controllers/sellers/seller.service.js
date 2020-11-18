6074aca024a413334d1e692c7c989f5b1dc2e31d
Prince Singh <prince.singh@telaminfotech.com>
2020-11-18 22:52:43 +0530

    Cheking data

--- controllers/sellers/seller.service.js
+++ controllers/sellers/seller.service.js
@@ -353,7 +353,7 @@ async function insertRecord(fieldsToBeInsert, fieldValues, tableName){
 
 
 async function add(req) {
-    console.log(req.body);
+    console.log(req.body)
     try {
         if (!_.isEmpty(req.body)) {
             // Insert new Delear
@@ -367,7 +367,7 @@ async function add(req) {
             if(is_Validate){
 
                 var competitor__c = null, owner_name__c = null, owner_phone__c = null, gstin__c = null, billingcity = null,  billingstreet = null,  billingpostalcode = null,  billingcountry = null, billingstate = null ,
-                name = null,   type1__c = 'Retailer', email__c=null, mobile_contact__c=null, potential_value__c=null, potential_retailer__c=null, dealer__c=null,category__c=null,retailer_category__c=null,area__c=null,asm_id=null;
+                name = null,   type1__c = 'Retailer', RecordTypeId ='0122w000000NfMlAAK' ,email__c=null, mobile_contact__c=null, potential_value__c=null, potential_retailer__c=null, dealer__c=null,category__c=null,retailer_category__c=null,area__c=null,asm_id=null;
                 var psm_id = null;
                 var asm_id = null;
                 myDetails = await db.agentDetail(req.headers.agentid);
@@ -436,9 +436,9 @@ async function add(req) {
                         
             
 
-                fieldsToBeInsert = ' asm__c, name,   type1__c, email__c, mobile_contact__c, potential_value__c, potential_retailer__c,category__c,retailer_category__c,createddate,competitor__c,owner_name__c,owner_phone__c,gstin__c,billingcity,billingstreet,billingpostalcode,billingcountry,billingstate,area__c,psm__c';
+                fieldsToBeInsert = ' asm__c, name,   type1__c,RecordTypeId, email__c, mobile_contact__c, potential_value__c, potential_retailer__c,category__c,retailer_category__c,createddate,competitor__c,owner_name__c,owner_phone__c,gstin__c,billingcity,billingstreet,billingpostalcode,billingcountry,billingstate,area__c,psm__c';
                 pg_id__c = uuidv4();
-                fieldValues = [ asm_id, req.body.name, '0122w000000NfMlAAK', email__c, mobile_contact__c, potential_value__c, potential_retailer__c, category__c, retailer_category__c, current_date_time, competitor__c, owner_name__c, owner_phone__c, gstin__c, billingcity, billingstreet, billingpostalcode, billingcountry, billingstate,area__c,psm_id];
+                fieldValues = [ asm_id, req.body.name, 'Retailer','0122w000000NfMlAAK', email__c,  mobile_contact__c, potential_value__c, potential_retailer__c, category__c, retailer_category__c, current_date_time, competitor__c, owner_name__c, owner_phone__c, gstin__c, billingcity, billingstreet, billingpostalcode, billingcountry, billingstate,area__c,psm_id];
                 
                 tableName = 'Account';
 
