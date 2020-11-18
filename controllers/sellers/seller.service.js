f7ed9b8272e2d955cccef294689a514f3602bbe3
Prince Singh <prince.singh@telaminfotech.com>
2020-11-19 01:58:15 +0530

    Cheking data

--- controllers/sellers/seller.service.js
+++ controllers/sellers/seller.service.js
@@ -433,12 +433,13 @@ async function add(req) {
                 if(req.body.area__c!=undefined && req.body.area__c!=""){
                     area__c =  req.body.area__c;
                 }
-                        
-            
-
+                var id = "SELECT Id FROM salesforce.RecordType WHERE DeveloperName= 'Retailers_Sales_Service'";
+                var responseData = await client.query(id);
+                if(responseData)
+                    let ID = responseData.Id;
                 fieldsToBeInsert = ' asm__c, name,   type1__c,RecordTypeId, email__c, mobile_contact__c, potential_value__c, potential_retailer__c,category__c,retailer_category__c,createddate,competitor__c,owner_name__c,owner_phone__c,gstin__c,billingcity,billingstreet,billingpostalcode,billingcountry,billingstate,area__c,psm__c';
                 pg_id__c = uuidv4();
-                fieldValues = [ asm_id, req.body.name, 'Retailer','0122w000000NfMlAAK', email__c,  mobile_contact__c, potential_value__c, potential_retailer__c, category__c, retailer_category__c, current_date_time, competitor__c, owner_name__c, owner_phone__c, gstin__c, billingcity, billingstreet, billingpostalcode, billingcountry, billingstate,area__c,psm_id];
+                fieldValues = [ asm_id, req.body.name, 'Retailer',ID, email__c,  mobile_contact__c, potential_value__c, potential_retailer__c, category__c, retailer_category__c, current_date_time, competitor__c, owner_name__c, owner_phone__c, gstin__c, billingcity, billingstreet, billingpostalcode, billingcountry, billingstate,area__c,psm_id];
                 
                 tableName = 'Account';
 
