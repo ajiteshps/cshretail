9ca49cc2509595999810926826689c615fe8a2f4
Prince Singh <prince.singh@telaminfotech.com>
2020-11-19 14:47:14 +0530

    data

--- controllers/sellers/seller.service.js
+++ controllers/sellers/seller.service.js
@@ -453,7 +453,7 @@ async function add(req) {
               
                 fieldsToBeInsert = ' asm__c, name,   type1__c,RecordTypeId, email__c, mobile_contact__c, potential_value__c, potential_retailer__c,category__c,retailer_category__c,createddate,competitor__c,owner_name__c,owner_phone__c,gstin__c,billingcity,billingstreet,billingpostalcode,billingcountry,billingstate,area__c,psm__c';
                 pg_id__c = uuidv4();
-                fieldValues = [ asm_id, req.body.name, 'Retailer',ID, email__c,  mobile_contact__c, potential_value__c, potential_retailer__c, category__c, retailer_category__c, current_date_time, competitor__c, owner_name__c, owner_phone__c, gstin__c, billingcity, billingstreet, billingpostalcode, billingcountry, billingstate,area__c,psm_id];
+                fieldValues = [ asm_id, req.body.name,req.body.type1__c,ID, email__c,  mobile_contact__c, potential_value__c, potential_retailer__c, category__c, retailer_category__c, current_date_time, competitor__c, owner_name__c, owner_phone__c, gstin__c, billingcity, billingstreet, billingpostalcode, billingcountry, billingstate,area__c,psm_id];
                 
                 tableName = 'Account';
 
