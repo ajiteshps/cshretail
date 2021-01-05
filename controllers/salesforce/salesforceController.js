799b4772ddb54b716118449d499fc69aba9dacda
Prince Singh <prince.singh@telaminfotech.com>
2021-01-05 16:42:13 +0530

    check

--- controllers/salesforce/salesforceController.js
+++ controllers/salesforce/salesforceController.js
@@ -10,20 +10,36 @@ const SF_PASSWORD = "Pop5289689";
 const SF_TOKEN= "EOVVIxgfH8rxXBh0q2vUuKyLN";
 
 
-const conn = new jsforce.Connection({
-    loginUrl:SF_LOGIN_URL
-});
+// const conn = new jsforce.Connection({
+//     loginUrl:SF_LOGIN_URL
+// });
 
-conn.login(SF_USERNAME,SF_PASSWORD+SF_TOKEN, (err,result) => {
-    if(err)
-    res.send('Not Connected To SalesForce');
-    else
-    console.log('Connected to Salesforce');
-    console.log(result);
-});
+// conn.login(SF_USERNAME,SF_PASSWORD+SF_TOKEN, (err,result) => {
+//     if(err)
+//     res.send('Not Connected To SalesForce');
+//     else
+//     console.log('Connected to Salesforce');
+//     console.log(result);
+// });
 
 router.get('/check',(req,res)=>{
-    res.send('Salesforce');
+    const SF_LOGIN_URL = "https://test.salesforce.com";
+    const SF_USERNAME = "prince@telaminfotech.com";
+    const SF_PASSWORD = "Pop5289689";
+    const SF_TOKEN= "EOVVIxgfH8rxXBh0q2vUuKyLN";
+    
+    
+    const conn = new jsforce.Connection({
+        loginUrl:SF_LOGIN_URL
+    });
+    
+    conn.login(SF_USERNAME,SF_PASSWORD+SF_TOKEN, (err,result) => {
+        if(err)
+        res.send('Not Connected To SalesForce');
+        else
+        console.log('Connected to Salesforce');
+        console.log(result);
+    });
 });
 module.exports = router;
 
