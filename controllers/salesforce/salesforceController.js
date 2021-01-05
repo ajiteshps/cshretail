7057f3a687f516ba9ddc284489341a6c5f5ae0fe
Prince Singh <prince.singh@telaminfotech.com>
2021-01-05 16:52:45 +0530

    check

--- controllers/salesforce/salesforceController.js
+++ controllers/salesforce/salesforceController.js
@@ -8,22 +8,22 @@ const SF_LOGIN_URL = "https://irt-digitalanalytics.my.salesforce.com";
 const SF_USERNAME = "hrms@icon.com";
 const SF_PASSWORD = "SanaTan@123";
 
-const conn = new jsforce.Connection({
-    loginUrl:SF_LOGIN_URL
-});
 
-conn.login(SF_USERNAME,SF_PASSWORD+SF_TOKEN, (err,result) => {
-    if(err)
-    console.send('Not Connected To SalesForce');
-    else{
-    console.log('Connected to Salesforce');
-    console.log(result);
-    }
-   
-});
 
 router.get('/check',(req,res)=>{
-    res.send('Salesforce');
+    const conn = new jsforce.Connection({
+        loginUrl:SF_LOGIN_URL
+    });
+    
+    conn.login(SF_USERNAME,SF_PASSWORD+SF_TOKEN, (err,result) => {
+        if(err)
+        console.send('Not Connected To SalesForce');
+        else{
+        console.log('Connected to Salesforce');
+        console.log(result);
+        }
+       
+    });
 });
 module.exports = router;
 
