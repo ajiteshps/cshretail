786fb9036964ae8ed669f66b7d2bd74ebde9f539
Prince Singh <prince.singh@telaminfotech.com>
2021-01-07 12:36:47 +0530

    check

--- controllers/salesforce/salesforceController.js
+++ controllers/salesforce/salesforceController.js
@@ -9,21 +9,27 @@ const SF_USERNAME = "hrms@icon.com";
 const SF_PASSWORD = "SanaTan@123";
 
 
-
-router.get('/check',(req,res)=>{
+router.post('/check',async(req,res)=>{
     const conn = new jsforce.Connection({
         loginUrl:SF_LOGIN_URL
     });
-    
     conn.login(SF_USERNAME,SF_PASSWORD, (err,result) => {
         if(err)
         console.send('Not Connected To SalesForce');
         else{
         console.log('Connected to Salesforce');
-        console.log(result);
+        var body = { teamId: req.body.teamId, month : 'Jan' };
+        var data = await conn.apex.post("/api/ExpensePDF/", body, function(err,response) {
+            response.json(data);
+        });
+        
         }
-       
     });
+
+   
+
+
 });
+
 module.exports = router;
 
