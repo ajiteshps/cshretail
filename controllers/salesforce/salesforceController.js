832fe316136de212e0a1aa477c31bacd1f94d3bd
Prince Singh <prince.singh@telaminfotech.com>
2021-01-08 13:06:20 +0530

    check

--- controllers/salesforce/salesforceController.js
+++ controllers/salesforce/salesforceController.js
@@ -1,23 +1,22 @@
 const express = require('express');
+const router = express.Router();
 
 const jsforce = require('jsforce');
-
+// const app = express();
+router.post('/localPDFGeneration',localPDFGeneration);
+  
+module.exports = router;
 const SF_LOGIN_URL = "https://cse--herokusand.my.salesforce.com";
 const SF_USERNAME = "shashwat@cloud-icon.com.irtsandcsh.HerokuSand";
 const SF_PASSWORD = "irt@1234";
-
-const app = express();
     // conn.login(SF_USERNAME,SF_PASSWORD, (err,result) => {
     //     if(err)
     //     console.log('Not Connected To SalesForce',err);
     //     else
     //     console.log('Connected to Salesforce');
     //     });
-
-
 // router.post('/api/ExpensePDF/',async(req,res)=>
 // {
-    
 //     console.log(conn,'Connection');
 //     console.log(req.body);
 //     var body = { teamId: req.body.teamId, month : req.body.month };
@@ -28,10 +27,7 @@ const app = express();
 //         else
 //         res.json({ 'success': false, 'error': err});
 //     });
-        
-    
 // });
-
 // exports.localPDFGeneration = async (req, res, next) => {
 //     const conn = new jsforce.Connection({
 //         loginUrl:SF_LOGIN_URL
@@ -39,31 +35,14 @@ const app = express();
 //     await conn.login(SF_USERNAME,SF_PASSWORD);
 //     console.log(conn);
 //   };
-
-
-
-app.post('/localPDF', async (req,res) => {
+async function localPDFGeneration(req, res, next) {
+    console.log('Hiting Api');
     const conn =  new jsforce.Connection({
         loginUrl:SF_LOGIN_URL
     });
     await conn.login(SF_USERNAME,SF_PASSWORD);
     console.log(conn,'connection');
-});
-
-   
-// router.post('/localPDFGeneration',localPDFGeneration);
-
-// async function localPDFGeneration(req, res, next) {
-//     console.log('Hiting Api');
-//     const conn =  new jsforce.Connection({
-//         loginUrl:SF_LOGIN_URL
-//     });
-//     await conn.login(SF_USERNAME,SF_PASSWORD);
-//     console.log(conn,'connection');
-
-//   };
+  };
 
 
 
-
-module.exports = app;
