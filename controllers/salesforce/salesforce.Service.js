1da2b246e97b4fe7f249cad3ce6a256f59bb0476
Prince Singh <prince.singh@telaminfotech.com>
2021-01-08 15:13:12 +0530

    check

---
+++ controllers/salesforce/salesforce.Service.js
@@ -0,0 +1,36 @@
+const jsforce = require('jsforce');
+const SF_LOGIN_URL = "https://cse--herokusand.my.salesforce.com";
+const SF_USERNAME = "shashwat@cloud-icon.com.irtsandcsh.HerokuSand";
+const SF_PASSWORD = "irt@1234";
+module.exports = {
+    connection
+}
+async function connection() {
+
+       console.log('Hiting Api');
+       const conn =  new jsforce.Connection({
+           loginUrl:SF_LOGIN_URL
+         });
+        await conn.login(SF_USERNAME,SF_PASSWORD);
+         console.log(conn,'Connection');
+         return conn;
+
+        //  if(connection){
+        //     res.json({status:true,'message':'Connected to Salesforce'});
+        //     }
+        //     else{
+        //     res.json({status:false,'message':'Not Connected'});
+        //     }
+    //      var body = { teamId: req.body.teamId, month : req.body.month };
+
+    //      conn.apex.post("/api/ExpensePDF/",body)
+    //      .then(response => res.status(200).json({
+    //          status:true,
+    //          message:response.message
+    //      }))
+    //      .catch(err => res.status(400).json({
+    //         status:false,
+    //         message:err.message
+    // })) ;
+   
+  };
--- controllers/salesforce/salesforceController.js
+++ controllers/salesforce/salesforceController.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jsforce = require('jsforce');
 // const app = express();
+const salesforceService  = require('./salesforce.Service');
 router.post('/localPDFGeneration',localPDFGeneration);
 const SF_LOGIN_URL = "https://cse--herokusand.my.salesforce.com";
 const SF_USERNAME = "shashwat@cloud-icon.com.irtsandcsh.HerokuSand";
@@ -11,31 +12,39 @@ const SF_PASSWORD = "irt@1234";
 
 module.exports = router;
 
-function localPDFGeneration(req,res,next) {
-    console.log(req.body);
-       console.log('Hiting Api');
-       const conn =  new jsforce.Connection({
-           loginUrl:SF_LOGIN_URL
-         });
-       var connection  =  conn.login(SF_USERNAME,SF_PASSWORD);
-         console.log(conn,'Connection');
+ function localPDFGeneration(req,res,next) {
+    // console.log(req.body);
+    //    console.log('Hiting Api');
+    //    const conn =  new jsforce.Connection({
+    //        loginUrl:SF_LOGIN_URL
+    //      });
+    //      conn.login(SF_USERNAME,SF_PASSWORD);
+    //      console.log(conn,'Connection');
         //  if(connection){
         //     res.json({status:true,'message':'Connected to Salesforce'});
         //     }
         //     else{
         //     res.json({status:false,'message':'Not Connected'});
         //     }
-         var body = { teamId: req.body.teamId, month : req.body.month };
-
-         conn.apex.post("/api/ExpensePDF",body)
-         .then(response => res.status(200).json({
-             status:true,
-             message:response.message
-         }))
-         .catch(err => res.status(400).json({
-            status:false,
-            message:err.message
+        salesforceService.connection()
+        .then(conn => {
+            console.log(conn,'Connection VAR');
+            var body = { teamId: req.body.teamId, month : req.body.month };
+            conn.apex.post("/api/ExpensePDF/",body)
+            .then(response => res.status(200).json({
+                status:true,
+                message:response.message
+            }))
+            .catch(err => res.status(400).json({
+                status:false,
+                message:err.message
+        })) ;
+        })
+        .catch(err => res.status(400).json({
+           status:false,
+           message:err.message
     })) ;
+         
 }
        
 
