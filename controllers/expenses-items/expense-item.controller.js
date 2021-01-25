d9d1ae34a379c394b9ede03d23e12037017bf164
React Ninja <reactninja04@gmail.com>
2021-01-25 19:29:58 +0530

    Added console for request query

--- controllers/expenses-items/expense-item.controller.js
+++ controllers/expenses-items/expense-item.controller.js
@@ -101,11 +101,11 @@ function updateExpense(req, res, next) {
     try {
         
         if (!_.isEmpty(req.headers.agentid) && req.query.id != undefined) {
+            console.log(req,'Request Expense Update')
             expenseService.updateExpense(req)
                 .then(expenses => res.status(expenses.status).json(expenses.response))
                 .catch(err => next(err));
         } else {
-            console.log(req,'Request Expense Update')
             res.status(400).json({ "success": false, "message": "Mandatory parameter(s) are missing." });
         }
     } catch (e) {
