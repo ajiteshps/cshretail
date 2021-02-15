fc22e3bc69a49dc7164ce4c4a500ae175aefd350
React Ninja <reactninja04@gmail.com>
2021-02-15 14:27:10 +0530

    Reverted to intial commit

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ console.log('branch',process.env.DATABASE_URL)
 try {
   global.client = new pg.Client(`${process.env.DATABASE_URL}?ssl=true`);
   client.connect();
-  console.log(client,' Client')
 } catch (e) {
   console.log(`ERROR::: app.js >>> 14 >>> err `, e);
 }
